60300fa7a9baa31dc8795d0b5885a008dfa6d081
Futheme <kyoro_souza@hotmail.com>
2024-03-08 21:30:42 -0300

    ✨ add breakpoints tokens

---
+++ src/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  sm: "@media screen (min-width >= 640px)",
+  md: "@media screen (min-width >= 768px)",
+  lg: "@media screen (min-width >= 1024px)",
+  xl: "@media screen (min-width >= 1280px)",
+  "2xl": "@media screen (min-width >= 1536px)",
+};
