51d121b63bea615912b5d406f71fc3ca02c30c7f
Futheme <kyoro_souza@hotmail.com>
2024-03-12 15:20:57 -0300

    🎨 adjust how properties gets values

--- src/breakpoints.ts
+++ src/breakpoints.ts
@@ -1,7 +1,7 @@
 export const breakpoints = {
-  sm: "@media screen (min-width >= 640px)",
-  md: "@media screen (min-width >= 768px)",
-  lg: "@media screen (min-width >= 1024px)",
-  xl: "@media screen (min-width >= 1280px)",
-  "2xl": "@media screen (min-width >= 1536px)",
+  sm: { value: "@media screen (min-width >= 640px)" },
+  md: { value: "@media screen (min-width >= 768px)" },
+  lg: { value: "@media screen (min-width >= 1024px)" },
+  xl: { value: "@media screen (min-width >= 1280px)" },
+  "2xl": { value: "@media screen (min-width >= 1536px)" },
 };
--- src/radii.ts
+++ src/radii.ts
@@ -1,10 +1,10 @@
 export const radii = {
-  xs: "2px",
-  sm: "4px",
-  md: "6px",
-  lg: "8px",
-  xl: "12px",
-  "2xl": "16px",
-  "3xl": "24px",
-  full: "99999px",
+  xs: { value: "2px" },
+  sm: { value: "4px" },
+  md: { value: "6px" },
+  lg: { value: "8px" },
+  xl: { value: "12px" },
+  "2xl": { value: "16px" },
+  "3xl": { value: "24px" },
+  full: { value: "99999px" },
 };
